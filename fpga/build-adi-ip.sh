1e4ea448d48edf4da1afe91174ae3aad80d8831c
Praveen Prabhu <praveen@pentarisc.systems>
2026-02-02 17:44:39 +0530

    Fixed adi-hdl build shell script

--- fpga/build-adi-ip.sh
+++ fpga/build-adi-ip.sh
@@ -4,7 +4,8 @@
 # // SPDX-FileCopyrightText: 2025 Xianjun Jiao
 # // SPDX-License-Identifier: Apache-2.0 license
 
-XILINX_DIR=$1
+source ../helpers/env.sh
+source "$XILINX_VIVADO_ROOT/settings64.sh"
 git submodule init adi-hdl
 git submodule update adi-hdl
 cd ./adi-hdl/
@@ -12,7 +13,5 @@ git reset --hard
 git fetch
 git checkout main
 git reset --hard main
-#source $XILINX_DIR/Vivado/2022.2/settings64.sh
-source $XILINX_DIR/Vivado/settings64.sh
 cd library/
 make
